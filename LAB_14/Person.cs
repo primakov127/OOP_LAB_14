3d5e2c01b1858d1b315936263f72e4bfd707bad9
Max Primakov <primakov127@gmail.com>
2019-10-28 23:34:19 +0300

    Completed First Task

---
+++ LAB_14/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_14
+{
+    [Serializable]
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person()
+        {
+            Name = "Nope";
+            Age = 0;
+        }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
--- LAB_14/Program.cs
+++ LAB_14/Program.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.Text.Json;
+using System.Xml.Serialization;
 
 namespace LAB_14
 {
@@ -10,6 +15,83 @@ namespace LAB_14
     {
         static void Main(string[] args)
         {
+            #region 1
+
+            //Person man = new Person("Max", 18);
+
+            //BinaryFormatter formatter = new BinaryFormatter();
+            //using (FileStream fs = new FileStream(@"..\..\person.dat", FileMode.OpenOrCreate))
+            //{
+            //    formatter.Serialize(fs, man);
+            //}
+
+            //using (FileStream fs = new FileStream(@"..\..\person.dat", FileMode.OpenOrCreate))
+            //{
+            //    Person manNew = (Person)formatter.Deserialize(fs);
+
+            //    Console.WriteLine($"Name: {manNew.Name} Age: {manNew.Age}");
+            //}
+
+            #endregion 1
+
+            #region 2
+
+            //Person man = new Person("Max", 18);
+
+            //SoapFormatter formatter = new SoapFormatter();
+
+            //using (FileStream fs = new FileStream(@"..\..\person.soap", FileMode.OpenOrCreate))
+            //{
+            //    formatter.Serialize(fs, man);
+            //}
+
+            //using (FileStream fs = new FileStream(@"..\..\person.soap", FileMode.OpenOrCreate))
+            //{
+            //    Person manNew = (Person)formatter.Deserialize(fs);
+
+            //    Console.WriteLine($"Name: {manNew.Name} Age: {manNew.Age}");
+            //}
+
+            #endregion 2
+
+            #region 3
+
+            //Person man = new Person("Max", 18);
+
+            //string json = JsonSerializer.Serialize<Person>(man);
+
+            //Console.WriteLine(json);
+
+            //Person manNew = JsonSerializer.Deserialize<Person>(json);
+            //Console.WriteLine($"Name: {manNew.Name} Age: {manNew.Age}");
+
+            #endregion 3
+
+            #region 4
+
+            Person man = new Person("Max", 18);
+
+            XmlSerializer formatter = new XmlSerializer(typeof(Person));
+
+            // получаем поток, куда будем записывать сериализованный объект
+            using (FileStream fs = new FileStream(@"..\..\persons.xml", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, man);
+
+                Console.WriteLine("Объект сериализован");
+            }
+
+            // десериализация
+            using (FileStream fs = new FileStream(@"..\..\persons.xml", FileMode.OpenOrCreate))
+            {
+                Person newPerson = (Person)formatter.Deserialize(fs);
+
+                Console.WriteLine("Объект десериализован");
+                Console.WriteLine($"Имя: {newPerson.Name} --- Возраст: {newPerson.Age}");
+            }
+
+            #endregion 4
+
         }
     }
 }
